8460fe7f1bff1b53e5a267d82a8a9831404eb410
Anh-Thy Ho <anhthynho@yahoo.com>
2019-10-13 15:41:53 -0700

    adam updated apply
    updated apply function for value in closures

--- src/edu/sjsu/fwjs/Value.java
+++ src/edu/sjsu/fwjs/Value.java
@@ -91,7 +91,9 @@ class ClosureVal implements Value {
      * be bound to its matching argument and added to the new local environment.
      */
     public Value apply(List<Value> argVals) {
-        // YOUR CODE HERE
-        return null;
+    	  Environment env = new Environment(outerEnv);
+          for (int i = 0; i < argVals.size(); i++)
+              env.createVar(params.get(i), argVals.get(i));
+          return body.evaluate(env);
     }
 }
